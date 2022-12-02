2d6df1fc8f0a9c9a6b3e95708778f31ba17ead2d
Midah Pasche <pasche@b1-systems.de>
2022-12-02 12:08:54 +0100

    fix cache backend __unserialize

--- lib/Horde/Imap/Client/Cache/Backend.php
+++ lib/Horde/Imap/Client/Cache/Backend.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright 2013-2017 Horde LLC (http://www.horde.org/)
  *
@@ -173,6 +174,6 @@ abstract class Horde_Imap_Client_Cache_Backend implements Serializable
     public function __unserialize(array $data)
     {
         $this->_params = $data;
+        $this->_initOb();
     }
-
 }
